3023009562d77eb0419e1db2150b9e899e9bd081
admim <admim@admims-MBP.fritz.box>
2020-12-04 21:29:42 +0100

    add controller models

---
+++ app/controllers/todos_controller.rb
@@ -0,0 +1,22 @@
+class TodosController < ApplicationController
+  def home
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+  
+  def create
+  end
+  
+  def update
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
-  root 'application#hello'
+  root 'todos#home'
+
+  get '/home', to: 'todos#home', as: "home"
+
+  post '/create', to: 'todos#create', as: "create"
+
+  get '/show/:id', to: 'todos#show', as: "show"
+
+  put '/todo/:id', to: 'todos#update', as: "push"
+
+  patch '/update/:id', to: 'todos#update', as: "update"
+
+  delete '/todo/:id', to: 'todos#destroy', as: "delete"
+
+  get '/new', to: 'todos#new', as: "new"
+
+  get '/edit/:id', to: 'todos#edit', as: "edit"
 end
