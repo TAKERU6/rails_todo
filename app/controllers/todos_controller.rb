e864a91de35219ab84c63503bd4cedb48fe1efb1
admim <admim@admims-MBP.fritz.box>
2020-12-05 20:33:02 +0100

    add model validates

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,11 +1,14 @@
 class TodosController < ApplicationController
   def home
+    @todos = Todo.all
   end
 
   def show
+    @todo = Todo.find(params[:id])
   end
 
   def new
+    @todo = Todo.new
   end
 
   def edit
